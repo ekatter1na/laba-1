cccd8617ad04865d7e9bfe6891608c69477b7fb7
ekatter1na <98614159+ekatter1na@users.noreply.github.com>
2023-02-16 08:39:13 +0300

    Add files via upload

---
+++ laba 1/laba 1/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace laba_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите количество шагов, если оно будет отрицательным, то программа закроется");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите число x:");
+            double x = Convert.ToDouble(Console.ReadLine());
+            double S = 0;
+            for (int i = 0; i <= n; i++)
+            {
+                if (n < 0)
+                {
+                    Console.WriteLine("Вы ввели отрицательное число шагов");
+                    break;
+                }
+                else
+                {
+                    x = (Math.Pow(-1, i)) * (Math.Pow(x, (i + 1))) / (i + 1);
+                    S += x;
+                    Console.WriteLine(S);
+                }
+            }
+        }
+    }
+}
